c43b2fd800f54ef65f966478fae164ac6431fc40
cwknapp144 <snknapp@gmail.com>
2025-10-15 00:10:13 -0500

    order.js final product

--- static/js/order.js
+++ static/js/order.js
@@ -1,23 +1,48 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+    
+    /*console.log("We made it to js");*/
+
     let product = document.getElementById("product");
     let quantity = document.getElementById("quantity");
     let total = document.getElementById("total_cost");
+    let update = document.getElementById("updateCost");
 
-    let p = product.value;
-    let q = quantity.value;
     
-    if (p === "p1"){
-        total.textContent = 18*q;
+
+
+    function change(){
+
+
+        let p = product.value;
+        let q = parseFloat(quantity.value);
+        
+        /* Just scan the product and multiply with some nice formatting
+            UPDATE: I needed to add a separate input field for update.
+            total is just what displays to the user.        
+        */
+        if (p === "p1"){
+            total.textContent = `Your total is: $${18.00*q}`;
+            update.value = 18.00*q;
+        }
+        else if (p === "p2"){
+            total.textContent = `Your total is: $${14.00*q}`;
+            update.value = 14.00*q;
+        }
+        else if (p === "p3"){
+            total.textContent = `Your total is: $${13.00*q}`;
+            update.value = 13.00*q;
+        }
+        else if (p === "p4"){
+            total.textContent = `Your total is: $${13.00*q}`;
+            update.value = 13.00*q;
+        }
     }
-    if (p === "p2"){
-        total.textContent = 14*q;
-    }
-    if (p === "p3"){
-        total.textContent = 13*q;
-    }
-    if (p === "p4"){
-        total.textContent = 13*q;
-    }
+
+        
+
+    product.addEventListener("change",change);
+    quantity.addEventListener("input",change);
+
 
 });
